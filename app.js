32bdaf38d4b73ec1be54c10b5274fc101c156a4f
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-16 18:35:40 +0530

    getelements by tag name

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-let smallimages = document.getElementsByClassName("oldImg");
+// let smallimages = document.getElementsByClassName("oldImg");
 
-for(let i = 0;i<smallimages.length;i++){
-    smallimages[i].src = "/Assets/Spider-Man.png";
-    console.log(" value of src changed");
-}
+// for(let i = 0;i<smallimages.length;i++){
+//     smallimages[i].src = "/Assets/Spider-Man.png";
+//     console.log(" value of src changed");
+// }
+//getelements by tag name
+// console.log(document.getElementsByTagName("p"));
+// document.getElementsByTagName("p")[1].innerText = "CG Fucked!!!!";
+//Query Selectors
+console.log(document.querySelector('p'));
